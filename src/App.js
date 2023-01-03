4991839db808075e2f8042dfbce32f70908b75a7
StefanDsd <stefan_dascalescu92@gmail.com>
2023-01-03 17:25:57 +0100

    Frontend Upload-File

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+
+  const [file, setFile] = useState();
+
+function handleChange(event) {
+  setFile(event.target.files[0]);
+}
+
+function handleSubmit() {
+  const url = "http://localhost:4000/uploads";
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("fileName", file.name);
+  const config = {
+    header: {
+      "content-type": 'multipart/form-data',
+    }
+  };
+  axios.post(url, formData, config).then((response) => {
+    console.log(response.data);
+  });
+  
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1>File Upload Stefan Dascalescu</h1>
+        <div className="input-group">
+                <label for='name'>Your name</label>
+                <input name='name' id='name' placeholder="Enter your name" />
+            </div>
+            <div className="input-group">
+                <label for='files'>Select files</label>
+                <input id='files' type="file" onChange={handleChange} />
+            </div>
+            <button class="submit-btn" onClick={handleSubmit}>Upload</button>
     </div>
   );
 }
